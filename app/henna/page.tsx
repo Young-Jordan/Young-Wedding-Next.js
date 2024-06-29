98e4bb917521f73bb2f1341aade237ceba9d8174
Jordan Young <jordan@driver-digital.com>
2024-06-29 15:43:09 -0400

    Added Henna RSVP Page

--- app/henna/page.tsx
+++ app/henna/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 import Script from "next/script"
 
-export default function RSVP() {
+export default function HENNA() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-center lg:p-24 p-12">
+            <h2 className="mb-6"> Henna RSVP </h2>
       <div className=" max-w-sm w-full opacity-0 iframe-contianer transition-opacity delay-500">
-      <iframe data-tally-src="https://tally.so/embed/nrKNKl?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="301" title="null"></iframe>
+      <iframe data-tally-src="https://tally.so/embed/3jyGy4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="301" title="null"></iframe>
       <Script
                 id="tally-js"
                 src="https://tally.so/widgets/embed.js"
