6f30cbdb57b7fad53025b377993cd90a8144f3af
Jordan Young <jordan@driver-digital.com>
2024-08-10 10:39:21 -0400

    Fixed Henna Forms

---
+++ app/details/components/Venue copy.tsx
@@ -0,0 +1,47 @@
+import Appear from "@/app/components/Appear";
+import Link from "next/link";
+import React from "react";
+
+export default function Venue() {
+  return (
+    <section className="flex snap-center min-h-screen flex-col items-center text-center justify-start lg:gap-y-12">
+      <div className="flex flex-col items-start lg:mt-0 mt-12 lg:h-[40vh] w-screen relative overflow-hidden">
+        <h1 className="font-serif absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          The Venue
+        </h1>
+        <video
+          width="1920"
+          height="1080"
+          className="opacity-40 grayscale w-full h-full object-cover object-center"
+          muted
+          loop
+          autoPlay
+          playsInline
+          preload="true"
+        >
+          <source src="/Bordone.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="p-12 max-w-2xl">
+        <h2 className="mb-4">The Bordone LIC</h2>
+        <p>
+          The Bordone LIC, a premier event space in Long Island City, boasts
+          breathtaking skyline views, a spacious outdoor terrace, and a modern,
+          versatile interior. We are so excited to be able to host everyone here
+          for both the ceremony & reception.
+        </p>
+        <Appear>
+          <Link
+            href={
+              "https://www.google.com/maps/dir//google+maps+the+bordone/@40.7523097,-73.9912983,13z/data=!3m1!5s0x89c258d8509de8d1:0xcc25649e3ec9acdc!4m8!4m7!1m0!1m5!1m1!1s0x89c259a1be533335:0x35f4014c71d0ac93!2m2!1d-73.9500988!2d40.752252?entry=ttu"
+            }
+            target="_blank"
+            className="outline-accent outline text-accent rounded-full w-fit uppercase mx-auto lg:mt-12 mt-6 lg:px-6 px-4 lg:py-3 py-1 lg:text-lg text-base tracking-widest hover:outline-0 hover:text-white hover:bg-black transition-colors"
+          >
+            Get Directions
+          </Link>
+        </Appear>
+      </div>
+    </section>
+  );
+}
--- app/henna/page.tsx
+++ app/henna/page.tsx
@@ -1,27 +1,38 @@
-'use client'
-import Script from "next/script"
+"use client";
+import Script from "next/script";
 
 export default function HENNA() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-center lg:p-24 p-12">
-            <h2 className="mb-6"> Henna RSVP </h2>
+      <h2 className="mb-6"> Henna RSVP </h2>
       <div className=" max-w-sm w-full opacity-0 iframe-contianer transition-opacity delay-500">
-      <iframe data-tally-src="https://tally.so/embed/3jyGy4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="301" title="null"></iframe>
-      <Script
-                id="tally-js"
-                src="https://tally.so/widgets/embed.js"
-                onLoad={() => {
-                    /* ts-ignore */
-                    console.log('trigger load embed');
-                    document.querySelector(".iframe-contianer")?.classList.add("opacity-100");
-                    /* ts-ignore */
-                    //Tally.loadEmbeds();
-                }}
-            />
+        <iframe
+          data-tally-src="https://tally.so/r/nrKNKlalignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+          loading="eager"
+          width="100%"
+          height="301"
+          title="null"
+        ></iframe>
+        <Script
+          id="tally-js"
+          src="https://tally.so/widgets/embed.js"
+          onLoad={() => {
+            /* ts-ignore */
+            console.log("trigger load embed");
+            document
+              .querySelector(".iframe-contianer")
+              ?.classList.add("opacity-100");
+            /* ts-ignore */
+            //Tally.loadEmbeds();
+          }}
+        />
 
-
-        <Script id="tally-script">{"/* ts-ignore */console.log('trigger load embed');Tally.loadEmbeds();document.querySelector('.iframe-contianer')?.classList.add('opacity-100');"}</Script>
+        <Script id="tally-script">
+          {
+            "/* ts-ignore */console.log('trigger load embed');Tally.loadEmbeds();document.querySelector('.iframe-contianer')?.classList.add('opacity-100');"
+          }
+        </Script>
       </div>
     </section>
-  )
+  );
 }
